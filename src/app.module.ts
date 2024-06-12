9fe80c7429746f551ef986cae4077d766b1aa2f1
NguyenHuuKienKiddiHub <kiennh.kiddihub@gmail.com>
2024-06-12 23:35:43 +0700

    change module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,12 @@
 import { Module } from '@nestjs/common';
-import { CategoryModule } from './category/category.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { DatabaseModule } from './core/database.module';
-import { NovelModule } from './novel/novel.module';
-import { AuthorModule } from './author/author.module';
-import { FeedbackModule } from './feedback/feedback.module';
+import { SitemapModule } from './sitemap/sitemap.module';
 
 const cronModules =
   process.env.APP_CRON === 'true' ? [ScheduleModule.forRoot()] : [];
 
 @Module({
-  imports: [
-    DatabaseModule,
-    ...cronModules,
-    CategoryModule,
-    NovelModule,
-    AuthorModule,
-    FeedbackModule,
-  ],
+  imports: [DatabaseModule, ...cronModules, SitemapModule],
 })
 export class AppModule {}
